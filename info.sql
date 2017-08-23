bb332042d1545b0e99226745582af1288503e986
Ananth-Alti <aramamurthy@altimetrik.com>
2017-08-23 22:59:19 +0530

    sql file added

---
+++ info.sql
@@ -0,0 +1,3 @@
+create database db;
+use db;
+create table info(username varchar(25),password varchar(25),firstname varchar(25),lastname varchar(25),address varchar(25),pincode int(11),phonenumber int(11));
